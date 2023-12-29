7f0309ba7a12f6488da33f2b9fe137c877c035c1
Andraxvpn <154669156+Andraxvpn@users.noreply.github.com>
2023-12-29 20:44:50 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+# Ganti dengan jalur direktori skrip Anda
+SCRIPT_DIR="/data/data/com.termux/files/home"
+
+# Ganti dengan URL repositori GitHub Anda
+REPO_URL="https://github.com/Andraxvpn/bash.git"
+
+cd "$SCRIPT_DIR" || exit 1
+
+# Mengambil pembaruan terbaru dari repositori GitHub
+git pull "$REPO_URL"
+
+echo "Skrip berhasil diperbarui."
 
